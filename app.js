973753c1cf8c6716632408a1a94d18b428810183
Haley <haleymlewis@gmail.com>
2019-02-16 09:41:59 -0800

    second object complete

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 
 var hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm', '8pm'];
 var pikeUl = document.getElementById('pike');
+var airportUl = document.getElementById('airport');
 
 var firstPike = {
     minHrlyCust: 23,
@@ -20,3 +21,21 @@ var firstPike = {
     }
 }
 firstPike.render();
+
+var seaTac = {
+    minHrlyCust: 3,
+    maxHrlyCust: 24,
+    avgCookie: 1.2,
+    random: function getRandomNumber(){
+        // console.log('random');
+        return Math.random() * ((this.minHrlyCust, this.maxHrlyCust) * this.avgCookie);
+    },
+    render: function(){
+        for(var i = 0; i < hours.length; i++){
+            var liEl = document.createElement('li');
+            liEl.textContent = hours[i] + ': ' + this.random().toFixed(0) + ' cookies.';
+            airportUl.appendChild(liEl);
+        }
+    }
+}
+seaTac.render();
